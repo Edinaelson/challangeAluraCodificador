476ebce381dd8910c059851d8cb8c7671e35e0a1
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-01-09 21:52:29 -0300

    crição funcão copiar, criptografar, layout

---
+++ calculation.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector("#send");
+    btn.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        //var text = document.getElementById('texto').innerHTML;
+        //document.writeln(text);
+
+        const text = document.querySelector("#texto");
+        const value = text.value;
+        const array = value.split('');
+        
+            //document.writeln(value);
+
+        for(var i = 0; i <= value.length; i++){
+            if (array[i] == "e"){
+                array[i] = "enter";
+            }
+            if (array[i] == "i"){
+                array[i] = "imes";
+            }
+            if (array[i] == "a"){
+                array[i] = "ai";
+            }
+            if (array[i] == "o"){
+                array[i] = "ober";
+            }
+            if (array[i] == "u"){
+                array[i] = "ufat";
+            }
+
+        }
+
+        document.getElementById('texto_recebido').innerHTML = array.join('');
+
+    });
